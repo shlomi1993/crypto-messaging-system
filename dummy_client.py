c14891cd28883728d01cca87001bad7cb27fb989
shlomi1993 <shlomiggl@gmail.com>
2021-06-22 19:15:30 +0300

    Finished server - not tested enough

--- dummy_client.py
+++ dummy_client.py
@@ -11,9 +11,12 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from cryptography.hazmat.primitives.serialization import load_pem_public_key
 
+password = b"password"
+salt = b"password"
+
 def Enc(key, data_string):
     cipher = Fernet(key)
-    return str(cipher.encrypt(data_string.encode()))
+    return cipher.encrypt(data_string.encode())
     
 # Encryption the message with Symmetric key
 def encryptionByKey(key, message):
@@ -24,6 +27,26 @@ def encryptionByKey(key, message):
                                  label=None)
                              )
     return ciphertext
+
+def genSymmetricKey(password, salt):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    return Fernet(key)
+
+# Convert IP and Port from string to bytes
+def convertIPandPORT(ip,port):
+    strIpArr = ip.split('.')
+    ipArr = [int(str) for str in strIpArr]
+    ip = bytes(ipArr)
+    port = int(port.rstrip())
+    port = (port).to_bytes(2, 'big')
+    return ip,port
 
 def handlePKFile(n):
     # try to load pk2 file
@@ -38,25 +61,28 @@ def handlePKFile(n):
     return pk
 
 TCP_IP = "127.0.0.1"
-TCP_PORT = 4002
+TCP_PORT = "5000"
 BUFFER_SIZE = 1024
 
-data = "cccc"
-k = Fernet.generate_key()
-c = Enc(k, data)
-msg = TCP_IP + str(TCP_PORT) + c
+x,y = convertIPandPORT(TCP_IP, TCP_PORT)
 
-print(msg)
-print("\n")
+message = "cccc"
+k = genSymmetricKey(password, salt)
+c = k.encrypt(message.encode())
+
+msg = x + y + c
+
+# print(msg)
 
 pk = handlePKFile(str(2))
-l = encryptionByKey(pk, msg.encode())
-
+l = encryptionByKey(pk, msg)
 
 print("sending l: " + str(l))    
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
+s.connect((TCP_IP, int(TCP_PORT)))
 s.send(l)
 s.close()
+
+
 
--- mix.py
+++ mix.py
@@ -1,87 +1,75 @@
-import socket, sys
+# Shlomi Ben-Shushan, 311408264, Ofir Ben-Ezra, 206073488
+
+import socket, sys, random
 from datetime import datetime
-import hashlib, base64
-from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
-from cryptography.hazmat.primitives.serialization import load_pem_public_key
 
+# Parse the given number
 try:
 	number = int(sys.argv[1])
 except:
     exit(-1)
-port = 4000 + number
+port = 8999 + number
 
+# Open server's socket.
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', port))
 s.listen(5)
 conn, addr = s.accept()
 conn.settimeout(2)
 
+# Get private key.
 with open("sk" + str(number) + ".pem", "rb") as skey:
 	sk = load_pem_private_key(skey.read(), password = None, backend = default_backend())
+ 
+# Set array of deliveries -- each delivery is a touple of [IP, Port, Message].
+deliveries = []
 
-def send_messages():
-    print("Send stuff")
-
-messages = []
-
+# Server's main loop.
 doing = False
 while True:
     
+    # Recieve data.
     data = conn.recv(4096)
     
+    # Once data recieved, decrypt and parse it.
     if len(data) > 0:
-        # print(data)
-        # size = len(data)
-        # bip = bytearray(size)
-        # bport = bytearray(size)
-        # bmsg = bytearray(size)
         
-        # print(type(data))
-        
-        # ip = data[:4]
-        # i = 0
-        # while i < len(ip):
-        #     bip[i] = ip[i]
-        #     i += 1
-        
-        # bip = bytes(bip)
-        # bport = bytes(bport)
-        # bmsg = bytes(bmsg)
-            
-        # i = 0
-        # while i < len(bip):
-        #     print(bip[i])
-        # print("\n\n\n\n")
-        # print(bip)
-        # port = data[4:6]
-        # msg = data[6:]
-        
-        # print(ip)
-        # print(port)
-        # print(msg)
-        
+        # Decryption. 
         plaintext = sk.decrypt(data,
 				padding.OAEP(mgf = padding.MGF1(algorithm = hashes.SHA256()),
 					algorithm = hashes.SHA256(),
 					label = None)
 				)
         
-
-        print(plaintext)
-        messages.append(plaintext)
-    
-    
-    time = datetime.now().strftime("%G:%M:%S")
-    time_splitted = time.split(":")
-    
-    if time_splitted != 00:
-        doing = False
+        # Parse IP.
+        ip = plaintext[:4]
+        ip = str(ip[0]) + "." + str(ip[1]) + "." + str(ip[2]) + "." + str(ip[3])
         
+        # Parse port.
+        port = plaintext[4:6]
+        port = int(hex(port[0])[2:] + hex(port[1])[2:], 16)
+        
+        # Parse message.
+        msg = plaintext[6:]   
+         
+        # Add new delivery to deliveries.
+        deliveries.append([ip, port, msg])
+    
+    # Check time.
+    time_splitted = datetime.now().strftime("%H:%M:%S").split(":")
+    
+    # Each rounded minute, pick a random delivery and send it over TCP to the next server\client.
+    if time_splitted[2] != "00":
+        doing = False
     elif doing == False:
-        send_messages()
+        if len(delivery) > 0:
+            delivery = random.choice(deliveries)
+            t = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            t.connect((delivery[0], delivery[1]))
+            t.send(delivery[2])
+            t.close()
